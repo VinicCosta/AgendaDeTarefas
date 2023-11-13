d56588e519ec5fd71ecdf180ac1ba4ca3369b675
Vinicius Costa <viniciuscodp@gmail.com>
2023-11-12 23:36:52 -0300

    Refatoração de código e incremento nos cards de tarefas.

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AtividadeForm from './components/AtividadeForm';
+import AtividadeLista from './components/AtividadeLista';
+
+let initialState = [
+  {
+    id: 1,
+    prioridade: '1',
+    titulo: "Título",
+    descricao: 'Primeira Atividade',
+  },
+  {
+    id: 2,
+    prioridade: '2',
+    titulo: "Título",
+    descricao: 'Segunda Atividade',
+  },
+  {
+    id: 3,
+    prioridade: '3',
+    titulo: "Título",
+    descricao: 'Terceira Atividade',
+  },
+]
 
 function App() {
+  const [atividades, setAtividades] = useState(initialState);
+
+  function addAtividades(e){
+    e.preventDefault();
+
+    const atividade = {
+      id: Number(document.getElementById('id').value),
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
+      descricao: document.getElementById('descricao').value,
+    };
+
+    setAtividades([...atividades, {...atividade}]);
+  }
+
+  function deletarAtividade(id){
+    const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+    setAtividades([...atividadesFiltradas]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React Agora
-        </a>
-      </header>
-    </div>
+    <>
+        <AtividadeForm 
+            atividades={atividades}
+            addAtividades={addAtividades}
+        />
+        <AtividadeLista 
+        atividades={atividades}
+        deletarAtividade={deletarAtividade}
+        />
+    </>
   );
 }
 
---
+++ pro-atividade-app/src/components/Atividade.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function Atividade(props) {
+
+  function prioridadeLabel(param){
+    switch(param){
+      case '1':
+        return 'Baixa';
+      case '2':
+        return 'Normal';
+      case '3':
+        return 'Alta';
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function prioridadeStyle(param, icone){
+    switch(param){
+      case '1':
+        return icone ? 'smile' : "success";
+      case '2':
+        return icone ? 'meh' : 'dark';
+      case '3':
+        return icone ? 'frown' : 'danger';
+      default:
+        return 'Não definido';
+      }
+    } 
+
+  return (
+    <div className={"card mb-2 shadow-sm border-" + prioridadeStyle(props.ativ.prioridade)}>
+                <div className='card-body'>
+                    <div className='d-flex justify-content-between'>
+                        <h5 className='card-title'>
+                          <span className='badge rounded-pill bg-secondary'>{props.ativ.id}</span> - {props.ativ.titulo}
+                        </h5>
+                        <h6>
+                          Prioridade: 
+                          <span className={'ms-1 text-'+prioridadeStyle(props.ativ.prioridade)}>
+                          <i className={'me-1 fa-regular fa-' + prioridadeStyle(props.ativ.prioridade,true)}></i>
+                          {prioridadeLabel(props.ativ.prioridade)}
+                          </span>
+                        </h6>
+                    </div>
+                    <p className='card-text'>{props.ativ.descricao}</p>
+                    <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+                        <button className='btn btn-sm btn-outline-primary me-2'>
+                          <i className='me-1 fas fa-pen'></i>
+                          Editar
+                        </button>
+                        <button className='btn btn-sm btn-outline-danger' onClick={() => props.deletarAtividade(props.ativ.id)}>
+                          <i className='me-1 fas fa-trash'></i>
+                          Deletar
+                        </button>
+                    </div> 
+                </div>
+              </div>
+  )
+}
